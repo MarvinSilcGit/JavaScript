b678e8e1f38c2c84dd73c2c695b5bd4f4e5e4f95
marvin <marvinsilc@protonmail.ch>
2025-01-09 16:39:09 -0300

    Resolving python challenges

---
+++ Functions/Matemáticas.js
@@ -0,0 +1,58 @@
+function resto_divisao(valor1, valor2)
+{
+    let resto = 0;
+
+    if (valor1 === 0 || valor2 === 0)
+    {
+
+        return "Divisão por zero inválida";
+
+    }else
+    {
+
+        if (valor1 === valor2)
+        {
+
+            resto = 0;
+
+        }
+
+        else if (valor1 > valor2)
+        {
+
+            while (valor2 + resto <= valor1)
+            {
+
+                resto += valor2;
+
+            }
+
+            resto = valor1 - resto;
+
+        }
+
+        else
+        {
+            while (valor1 + resto < valor2)
+            {
+
+                resto +=valor1;
+
+            }
+
+            while (resto !== valor2)
+            {
+                resto += 1;
+            }
+
+        }
+
+        return `O resto da divisão entre ${valor1} e ${valor2} é: ${resto}`;
+    }
+
+}
+
+for (let contador = 0; contador < 10; contador++)
+{
+    console.log(resto_divisao(contador,4))
+}
