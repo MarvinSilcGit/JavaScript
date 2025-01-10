a2d047d77f470d5e20544e72d76d1eca4ffb91c4
marvin <marvinsilc@protonmail.ch>
2025-01-10 10:05:13 -0300

    Fixing indentation

--- Functions/Matemáticas.js
+++ Functions/Matemáticas.js
@@ -456,3 +456,34 @@ function calculo_fatorial(numero)
 
     return resultado_fatorial;
 }
+
+
+function raio_circulo(area)
+{
+
+    area = 3.14 * (area**2);
+
+    return area;
+
+}
+
+
+function fahrenheit_celsius(temperatura)
+{
+
+    let celsius = (temperatura - 32) / 1.8;
+
+    return `A temperatura em ${temperatura.toFixed(1)}° fahrenheit equivale à ${celsius.toFixed(1)}° celsius`;
+
+}
+
+
+function celsius_fahrenheit(temperatura)
+{
+
+    let fahrenheit = (temperatura * 1.8) + 32;
+
+    return `A temperatura em ${temperatura.toFixed(1)}° celsius equivale à ${fahrenheit.toFixed(1)}° fahrenheit`;
+}
+
+console.log(celsius_fahrenheit(122))
