1490f088f236594e8b1f9c3bfe320728d57b1cb0
marvin <marvinsilc@protonmail.ch>
2025-01-24 22:39:02 -0300

    Adding new python function

--- Functions/Matemáticas/Numbers.js
+++ Functions/Matemáticas/Numbers.js
@@ -383,18 +383,69 @@ function numero_fizz_buzz(numero)
 }
 
 
-function binary_decimal()
+function binary_decimal(numero)
 {
 
+    let tamanho_decimal = numero.length;
 
+    let decimal = 0;
+
+    let expoente = tamanho_decimal - 1;
+
+    for (let contador of numero)
+    {
+
+        if (contador === "1")
+        {
+
+            decimal += (2 ** expoente);
+
+        }
+
+        else if (contador === "0")
+        {
+
+            decimal += 0;
+
+        }
+
+        else
+        {
+
+            return 'Esse número não é binário';
+
+        }
+
+        expoente--;
+
+        numero = parseInt(numero);
+
+    }
+
+    return `${decimal}`;
 
 }
 
 
-function decimal_binary()
+function decimal_binary(numero)
 {
 
+    let resto = [];
 
+    let numero_decimal = parseInt(numero);
+
+    while (numero_decimal !== 0)
+    {
+
+        resto.unshift(numero_decimal % 2);
+
+        numero_decimal = Math.floor(numero_decimal / 2);
+
+    }
+
+    resto = resto.join('');
+
+    return `${resto}`;
 
 }
 
